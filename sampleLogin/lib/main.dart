edc016f67454519b9e13e3483d16a8d00fd14a9b
AnnRoseBenny <roseann2152@gmail.com>
2020-11-28 19:48:06 +0530

    more features

--- sampleLogin/lib/main.dart
+++ sampleLogin/lib/main.dart
@@ -1,21 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sampleLogin/loginUI.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sampleLogin/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: NewLoginPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: FirebaseAuth.instance.currentUser != null
+            ? HomePage()
+            : NewLoginPage());
   }
 }
